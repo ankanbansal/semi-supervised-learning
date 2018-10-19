021df4eabe7e03911fac8ce74900a74302c10fef
Ankan Bansal <ankan@vulcan09.umiacs.umd.edu>
2018-10-19 10:02:35 -0400

    cosine

--- CIFAR10/train_cifar10.sh
+++ CIFAR10/train_cifar10.sh
@@ -1,10 +1,13 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --log_dir \
-./sup_4k_tot_50k/logs_MEL_reg/sm/ratio0.7/alpha0.8_delta400.0/ \
-    --type cls_MEL_reg --val_on True --save_dir \
-./sup_4k_tot_50k/checkpoints_MEL_reg/sm/ratio0.7/alpha0.8_delta400.0/ \
+CUDA_VISIBLE_DEVICES=6 python main.py --log_dir \
+./sup_4k_tot_50k/logs_MEL_reg/sm/ratio0.7/cosine/alpha0.8_delta0.5/ \
+    --type cls_MEL_reg --reg_distance_type cosine --data_dir \
+/vulcan/scratch/ankan/Amazon/data/ \
+    --val_on True \
+    --save_dir \
+./sup_4k_tot_50k/checkpoints_MEL_reg/sm/ratio0.7/cosine/alpha0.8_delta0.5/ \
     --batch_size 128 \
     --num_workers 0 \
-    --delta 400.0 \
+    --delta 0.5 \
     --runs 5 \
     --lr_step 40 \
     --epochs 100 \
